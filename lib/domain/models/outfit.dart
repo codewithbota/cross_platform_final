b6c6a11d3ab3711f9e148923285560d35f88acb6
Nazly <naz.nazko01@gmail.com>
2026-05-19 17:33:22 +0500

    feat: Outfit + PlannerEntry модели + outfit/planner репозитории

---
+++ lib/domain/models/outfit.dart
@@ -0,0 +1,56 @@
+class Outfit {
+  final String id;
+  final String name;
+  final String topId;
+  final String bottomId;
+  final String shoesId;
+  final String? accessoryId;
+  final DateTime createdAt;
+  bool isFavorite;
+
+  final String top;
+  final String bottom;
+  final String shoes;
+  final String accessory;
+  final String topEmoji;
+  final String bottomEmoji;
+  final String shoesEmoji;
+
+  Outfit({
+    required this.id,
+    required this.name,
+    required this.topId,
+    required this.bottomId,
+    required this.shoesId,
+    this.accessoryId,
+    required this.createdAt,
+    this.isFavorite = false,
+    this.top = '',
+    this.bottom = '',
+    this.shoes = '',
+    this.accessory = '',
+    this.topEmoji = '👕',
+    this.bottomEmoji = '👖',
+    this.shoesEmoji = '👟',
+  });
+
+  Outfit copyWith({bool? isFavorite}) {
+    return Outfit(
+      id: id,
+      name: name,
+      topId: topId,
+      bottomId: bottomId,
+      shoesId: shoesId,
+      accessoryId: accessoryId,
+      createdAt: createdAt,
+      isFavorite: isFavorite ?? this.isFavorite,
+      top: top,
+      bottom: bottom,
+      shoes: shoes,
+      accessory: accessory,
+      topEmoji: topEmoji,
+      bottomEmoji: bottomEmoji,
+      shoesEmoji: shoesEmoji,
+    );
+  }
+}
