97f8a5a013e6b5427afb398e3132b20e7c72ae0d
Nazly <naz.nazko01@gmail.com>
2026-05-21 13:25:23 +0500

    add repositories

---
+++ lib/data/repositories/community_repository_impl.dart
@@ -0,0 +1,23 @@
+// lib/data/repositories/community_repository_impl.dart
+
+import '../../domain/models/community_post.dart';
+import '../../domain/repositories/community_repository.dart';
+import '../remote/firestore_service.dart';
+
+class CommunityRepositoryImpl implements CommunityRepository {
+  final FirestoreService _service;
+
+  CommunityRepositoryImpl(this._service);
+
+  @override
+  Stream<List<CommunityPost>> watchPosts(String? currentUserId) =>
+      _service.watchPosts(currentUserId);
+
+  @override
+  Future<void> createPost(CommunityPost post) => _service.createPost(post);
+
+  @override
+  Future<void> toggleLike(
+          String postId, bool isLiked, int currentLikes, String userId) =>
+      _service.toggleLike(postId, isLiked, currentLikes, userId);
+}
